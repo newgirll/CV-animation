6d2564dc87578c500199e99843688382de13d866
newgirll <k.kowalska2407@gmail.com>
2018-01-31 22:06:32 +0100

    add sections start, about

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,13 +1,39 @@
 
+const moveFigure = () => {
+    const leftLeg = $('#leftLeg');
+    const rightLeg = $('#rightLeg');
+    const box = $('.figure__box');
 
-const startGame = (btn, section) => {
- 
-    btn.on('click', (section) => {
-        $('.start').css('display', 'none');
-    } );
-
+  
+    box.animate({
+        display: 'block',
+        transform: 'rotate(100deg)',
+        top: '50%',
+        left: '40%',
+        opacity: 1
+    }, 4000)
+        
 }
 
+    
 
+const showFigure = () => {
+    const sec = $('.start');
+    sec.css('display', 'none');
+
+    const figure = $('#figure');
+    const about = $('#aboutSec');
+    moveFigure();
+    about.css('display', 'flex');
+    about.css('align-items', 'flex-end');
+  
+}
+
+const startGame = () => {
+  const startBtn = $('#btnStart');
+    startBtn.on('click', () => showFigure());
+
+
+}
 
 export default startGame;
