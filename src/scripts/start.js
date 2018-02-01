a4e5238eb7cd711617ea149e96251ad83575afd2
newgirll <k.kowalska2407@gmail.com>
2018-02-01 23:41:58 +0100

    change start function, add further and choice options

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -8,10 +8,12 @@ const moveFigure = () => {
     box.animate({
         display: 'block',
         transform: 'rotate(100deg)',
-        top: '50%',
-        left: '40%',
+        top: '60%',
+        left: '30%',
         opacity: 1
     }, 4000)
+
+  
         
 }
 
@@ -22,16 +24,31 @@ const showFigure = () => {
     sec.css('display', 'none');
 
     const figure = $('#figure');
-    const about = $('#aboutSec');
+   
     moveFigure();
-    about.css('display', 'flex');
-    about.css('align-items', 'flex-end');
+  
   
 }
 
 const startGame = () => {
   const startBtn = $('#btnStart');
+  const about = $('#aboutSec');
+  const startLink = $('#startLink');
+  const moveLink = $('#moveLink');
     startBtn.on('click', () => showFigure());
+    startLink.on('click', () => {
+        moveLink.css('opacity', '1');
+        about.css('display', 'flex');
+        about.css('align-items', 'flex-start');
+        
+    })
+    moveLink.on('click', () => {
+        const choice = $('#choiceSec');
+        console.log(choice);
+        about.css('display', 'none');
+        choice.css('display', 'block');
+
+    });
 
 
 }
